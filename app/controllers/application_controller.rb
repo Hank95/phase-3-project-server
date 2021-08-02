c49527a5c10afb0aef2f64fe2cdcc17737cd1511
Henry <hhpendleton@gmail.com>
2021-08-02 16:22:17 -0400

    methods for fetch calls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,25 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/bars" do
+    bars = Bar.all
+    bars.to_json(include: :bar_game_types)
   end
 
-  get "/bars" do 
-    bars = Bar.all
+  get '/game_types' do
+    game_types = BarGameType.all
+    game_types.to_json
+  end
+
+  # get "/bars" do 
+  #   bars = Bar.all
+  #   bars.to_json()
+  # end
+
+  get "/bars/:bar_game_types/:city" do
+    bars = Bar.where(params[:bar_game_types, :city])
     bars.to_json
   end
+
 
 end
